e5c03d30845eb91e352ff16e783494c8aecfb2f0
Hrishiraj18 <121205156+Hrishiraj18@users.noreply.github.com>
2023-09-20 07:21:22 +0530

    q6 added

--- Cycle 1/q4sin(x).py
+++ Cycle 1/q4sin(x).py
@@ -3,7 +3,7 @@ x=int(input("Enter the radian"))
 n=int(input("Enter the number"))
 def sum_sin(x,n):
     sum_sin=0
-    for i in range(0,n+1):
+    for i in range(0,n):
         sign=(-1)**i
         p=(x*math.pi)/180
         sum_sin+=((p**(2*i+1))/math.factorial(2*i+1))*sign
